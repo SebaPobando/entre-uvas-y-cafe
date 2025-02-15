15dcdd8b14a38e241168a789c9ab28f42faea92c
Sr Jengibre <spobleteobando@gmail.com>
2025-02-15 19:47:50 -0300

    ADD Contact Form to WSP

--- assets/javascript/contacto.js
+++ assets/javascript/contacto.js
@@ -12,3 +12,18 @@ sr.reveal('.container__contacto', { delay: 400 });
 sr.reveal('.contact__form', { interval: 200 });
 sr.reveal('.contact__input', { interval: 200 });
 sr.reveal('.contact__button', { delay: 600 });
+
+document.getElementById('enviar').addEventListener('click', function () {
+    var nombre = document.getElementById('nombre').value;
+    var mensaje = document.getElementById('mensaje').value;
+
+    console.log('Nombre:', nombre);
+    console.log('Mensaje:', mensaje);
+
+    var texto = `¡Hola!, me llamo ${nombre}, quería consultar ${mensaje}`;
+    var url = `https://wa.me/56984230531?text=${texto}`;
+
+    console.log('URL:', url);
+
+    window.open(url, '_blank');
+});
